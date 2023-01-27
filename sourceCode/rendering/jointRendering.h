e45388c4fa6192bb78f9ce875915672d00474996
Coppelia <mfreese@gmx.ch>
2023-01-27 15:00:33 +0100

    adjusted copyright and dates

--- sourceCode/rendering/jointRendering.h
+++ sourceCode/rendering/jointRendering.h
@@ -1,3 +1,23 @@
+/*
+Source code based on the V-REP library source code from Coppelia
+Robotics AG on September 2019
+
+Copyright (C) 2006-2023 Coppelia Robotics AG
+Copyright (C) 2019 Robot Nordic ApS
+
+All rights reserved.
+
+GNU GPL license:
+================
+The code in this file is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+See the GNU General Public License for more details.
+<http://www.gnu.org/licenses/>
+*/
+
 #include "app.h"
 #include "rendering.h"
 
