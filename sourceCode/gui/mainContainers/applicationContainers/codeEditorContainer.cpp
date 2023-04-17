16ffeba7218c712e3c9c2ff8aee7dc10585016df
Coppelia <mfreese@gmx.ch>
2023-04-17 14:48:08 +0200

    fixed script-up-to-date

--- sourceCode/gui/mainContainers/applicationContainers/codeEditorContainer.cpp
+++ sourceCode/gui/mainContainers/applicationContainers/codeEditorContainer.cpp
@@ -370,7 +370,7 @@ int CCodeEditorContainer::openSimulationScript(int scriptHandle,int callingScrip
                     editorNode->SetAttribute("searchable",toBoolStr(true));
                     editorNode->SetAttribute("line-numbers",toBoolStr(true));
                     editorNode->SetAttribute("tab-width",4);
-                    editorNode->SetAttribute("script-up-to-date",it->getIsUpToDate());
+                    editorNode->SetAttribute("script-up-to-date",toBoolStr(it->getIsUpToDate()));
                     if (it->getLanguage()==CScriptObject::lang_lua)
                     {
                         editorNode->SetAttribute("lang","lua");
@@ -499,7 +499,7 @@ int CCodeEditorContainer::openCustomizationScript(int scriptHandle,int callingSc
                     editorNode->SetAttribute("searchable",toBoolStr(true));
                     editorNode->SetAttribute("line-numbers",toBoolStr(true));
                     editorNode->SetAttribute("tab-width",4);
-                    editorNode->SetAttribute("script-up-to-date",it->getIsUpToDate());
+                    editorNode->SetAttribute("script-up-to-date",toBoolStr(it->getIsUpToDate()));
                     if (it->getLanguage()==CScriptObject::lang_lua)
                     {
                         editorNode->SetAttribute("lang","lua");
